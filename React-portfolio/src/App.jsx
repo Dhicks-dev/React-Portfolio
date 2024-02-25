ef207eaa966bea402b7e21187e182de0bd8e99ca
Deontae Hicks <72946905+Dhicks-dev@users.noreply.github.com>
2024-02-24 19:35:50 -0600

    created contact component

--- React-portfolio/src/App.jsx
+++ React-portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Sidenav from './components/Sidenav'
 import Main from './components/main'
 import Projects from './components/Projects'
+import Contact from './components/contact'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Sidenav />
         <Main   />
         <Projects />
+        <Contact />
     </div>
   )
 }
